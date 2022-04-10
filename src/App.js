22dd7fcdee3720c5a24c3bc06d22af722a851724
NiotechOnSpotify <nicokoeninger@arcor.de>
2022-04-10 18:26:16 +0200

    neue  änderungen

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ function App() {
   //const greeterAddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512"
   let [alphaNumerischeZahl, setZahl] = useState()
   const [waehrung, setWaehrung] = useState('Dollar');
-  const ausAddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512"
+  const ausAddress = "0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9"
 
   
   const options = [
